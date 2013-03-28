af3da0ffd11263dea8dfc4aa2282fc41b0a9b393
dl <444c43@gmail.com>
2013-03-28 12:59:17 -0400

    merge general libraries

---
+++ GeneralLibraries/InputService/Input.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InputService
+{
+    public class Input
+    {
+        public static string UserInput(string prompt)
+        {
+            Console.WriteLine(prompt);
+
+            return Console.ReadLine();
+        }
+    }
+}
---
+++ GeneralLibraries/ValidationService/Validation.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValidationService
+{
+    public class Validation
+    {
+        public static int ValidateAsInteger(string inputtext)
+        {
+            int result = 0;
+            if (int.TryParse(inputtext, out result))
+            {
+                return result;
+            }
+
+
+        }
+    }
+}
