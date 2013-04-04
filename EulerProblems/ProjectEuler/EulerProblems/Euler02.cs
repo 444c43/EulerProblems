a049f7b1b4b3389d75db764ad675bd1670ca85b1
Dubman <keithdublin@gmail.com>
2013-04-04 11:13:52 -0400

    Adding feedback on Project Euler Problems 1 and 2 Modularization.

--- EulerProblems/ProjectEuler/EulerProblems/Euler02.cs
+++ EulerProblems/ProjectEuler/EulerProblems/Euler02.cs
@@ -21,9 +21,11 @@ namespace EulerProblems
 
         private static string QueryUser()
         {
+            // Used a built in solution for New Line
+            // Also, maybe string builder would work.
             string[] EulerInstructions = new string[]{
                 "Let's solve Euler # 2!",
-                "\n", "\n",
+                Environment.NewLine, Environment.NewLine,
                 "Enter a maximum range value: "};
 
             return ValidationService.ValidateUserInput(EulerInstructions);
@@ -31,15 +33,22 @@ namespace EulerProblems
 
         private static int CalculateIterative(int maxRange)
         {
+            // I think the use of an array here might be hiding the meaning of
+            // the three variables.  Actual variables like first, second and total might be
+            // better.
+
             int SumTotal = 0;
             int[] FibSet = new int[2] { 1, 2 };
             int Temp;
-            while (FibSet[1] < maxRange)
+            while (LimitHasNotBeenReached(maxRange, FibSet[1]))
             {
                 if (IsNumberEven(FibSet[1]))
                 {
                     SumTotal += FibSet[1];
                 }
+
+                // A pretty sure there is a solution that doesn't require a temp variable.
+                // Take a few minutes and see if you can think of it.
                 Temp = FibSet[1];
                 FibSet[1] = FibSet[0]+ FibSet[1];
                 FibSet[0] = Temp;
@@ -47,19 +56,23 @@ namespace EulerProblems
             return SumTotal;
         }
 
+        private static bool LimitHasNotBeenReached(int maxRange, int current)
+        {
+            // No need to pass the array here, I changed it to just an integer.  That was this method
+            // doesn't need to be aware of the existence of an array.
+            return current < maxRange;
+        }
+
         private static int CalculateEfficient(int maxRange)
         {
-
+            // TODO: Efficient solution
             return 0;
         }
 
         private static bool IsNumberEven(int fibOne)
         {
-            if (fibOne % 2 == 0)
-            {
-                return true;
-            }
-            return false;
+            // Simplified.
+            return fibOne % 2 == 0;
         }
     }
 }
--- EulerProblems/ProjectEuler/Program.cs
+++ EulerProblems/ProjectEuler/Program.cs
@@ -10,6 +10,18 @@ namespace EulerProblems
     {
         static void Main(string[] args)
         {
+            /*  Comments from Keith 4/4/13
+             *  
+             * Nice work decomposing the problems.  I'd take things here 1 full step further.
+             * I'd try and get the average method length down to 4 lines or so.
+             * 
+             * I've added a bunch of suggested changes to Euler2.
+             * 
+             * Let me know if you have any questions.
+             * 
+             */
+
+
             Euler01.EulerMain();
             Console.Clear();
             Euler02.EulerMain();
