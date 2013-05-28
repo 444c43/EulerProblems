cefdec0b54df44bd212c304ca7ca473de77c6c9c
dl <444c43@gmail.com>
2013-05-28 13:16:23 -0400

    add unit tests project

---
+++ EulerTests/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EulerTests
+{
+    [TestClass]
+    public class TestEuler01
+    {
+        [TestMethod]
+        public void TestIterative()
+        {
+        }
+
+        [TestMethod]
+        public void TestEfficient()
+        {
+        }
+    }
+}
