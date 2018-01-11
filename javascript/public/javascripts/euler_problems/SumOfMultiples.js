24804455b8e3a99302bd074990ad8f3d3744ff3e
DL <444c43@gmail.com>
2018-01-11 01:05:53 -0500

    use correct, more reliable equality operator

--- javascript/public/javascripts/euler_problems/SumOfMultiples.js
+++ javascript/public/javascripts/euler_problems/SumOfMultiples.js
@@ -4,7 +4,7 @@ function SumOfMultiples(){
 SumOfMultiples.prototype.sum = function(maxNum) {
   var sum = 0;
   for (var i = 0; i < maxNum; i++) {
-    if (i % 3 == 0 || i % 5 == 0) {
+    if (i % 3 === 0 || i % 5 === 0) {
       sum += i; 
     }
   }
